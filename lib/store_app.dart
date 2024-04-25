e0c2c9ed97bda40bbac25d4e611d0f188fa94077
Mohammed FLITI <mohammedfliti02@gmail.com>
2024-04-25 19:03:17 +0100

    env created done

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/env.variables.dart';
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
@@ -6,6 +7,7 @@ class StoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Store App',
+      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
