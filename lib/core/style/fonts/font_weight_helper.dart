8563d5894c907688dd3e541e44a5a0b9b3169ae3
Mohammed FLITI <mohammedfliti02@gmail.com>
2024-04-30 23:18:55 +0100

    font familly done

---
+++ lib/core/style/fonts/font_weight_helper.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FontWeightHelper{
+
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight meduim = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w400;
+}
--- lib/store_app.dart
+++ lib/store_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env.variables.dart';
 import 'package:store_app/core/common/screens/no_network_screen.dart';
+import 'package:store_app/core/style/fonts/font_familly_helper.dart';
+import 'package:store_app/core/style/fonts/font_weight_helper.dart';
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
@@ -13,13 +15,14 @@ class StoreApp extends StatelessWidget {
         builder: (_, value, __) {
           if (value) {
             return ScreenUtilInit(
-              designSize: const Size(375,812),
-              minTextAdapt: true, 
+              designSize: const Size(375, 812),
+              minTextAdapt: true,
               child: MaterialApp(
                 title: 'Store App',
                 debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
                 theme: ThemeData(
-                  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                  colorScheme:
+                      ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                   useMaterial3: true,
                 ),
                 builder: (context, widget) {
@@ -35,6 +38,45 @@ class StoreApp extends StatelessWidget {
                 home: Scaffold(
                   appBar: AppBar(
                     title: const Text('Welcome'),
+                  ),
+                  body: const Center(
+                    child: Column(
+                      children: [
+                        // old
+                        Text(
+                          'old font',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          ' 1 x الحمدلله',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        // new
+                        Text(
+                          'new font',
+                          style: TextStyle(
+                            fontFamily: FontFamillyHelper.poppinsEnglish,
+                            fontWeight: FontWeightHelper.bold,
+                            fontSize: 20,
+                          ),
+                            
+                            
+                        ),
+                        Text(
+                          ' 2 x الحمدلله',
+                          style: TextStyle(
+                            fontFamily: FontFamillyHelper.cairoArabic,
+                            fontWeight: FontWeightHelper.regular,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
